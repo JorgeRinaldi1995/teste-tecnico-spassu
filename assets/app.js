55a850228509a6d596c162fd04ebcd37b39cf8c4
Jorge Costa <jorge.rinaldi.1995@gmail.com>
2026-06-18 14:52:24 -0300

    front end responsive adjustments

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,14 @@ import './stimulus_bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
 import './styles/app.css';
 import './styles/components/alert.css';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/components/forms.css';
+import './styles/components/table.css';
+
 import './js/alert.js';
 
 import { initLivroForm } from './js/livro-form.js';
